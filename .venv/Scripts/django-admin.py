6365c467655cd2aa42746d76ab8aeae7c465aaea
Navjot Sherry <navjotsherryy@gmail.com>
2022-01-16 02:40:58 +0530

    frontend

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Sherry\Django\Projects\TodoWithReact\.venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ React/frontend/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';  
+import './App.css';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className = "container">
+
+        <div id="task-container">
+          <div id="form-wrapper">
+            <div id="form">
+              <div className='flex-wrapper'>
+                <div style={{flex:6}}>
+                  <input className='form-control' id='title' type="text" name="title" placeholder='Add Task title here..'></input>
+                </div>
+                <div style={{flex:1}}>
+                  <input id="submit" className='bt btn-warning' type="submit" name='Add'></input>
+
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div id="list-wrapper">
+
+          </div>
+
+        </div>
+        
+      </div>
+    )
+  }
+}
+
+export default App;
