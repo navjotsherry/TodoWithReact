db53b2cfdaa945afdc23c6d840915e3ca6594a6d
Navjot Sherry <navjotsherryy@gmail.com>
2022-01-17 08:23:53 +0530

    Adding New Task

--- React/frontend/src/App.js
+++ React/frontend/src/App.js
@@ -15,6 +15,8 @@ class App extends React.Component{
         editing:false,
       };
       this.fetchTasks = this.fetchTasks.bind(this)
+      this.handleChange = this.handleChange.bind(this)
+      this.handleSubmit = this.handleSubmit.bind(this)
   };
 
   componentWillMount(){
@@ -32,6 +34,49 @@ class App extends React.Component{
       ) 
   }
 
+  handleChange(e){
+    var name= e.target.name
+    var value = e.target.value
+    console.log(name , value)
+
+    this.setState({
+      activeItem:{
+        ...this.state.activeItem,
+        title:value
+      }
+    })
+
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    console.log("Item: ",this.state.activeItem);
+
+    var url = 'http://127.0.0.1:8000/api/task-create/'
+    fetch(url,{
+      method : 'POST',
+      headers:{
+        'Content-type':'application/json',
+        
+      },
+      body:JSON.stringify(this.state.activeItem)
+    }).then((response)=>{
+      this.fetchTasks()
+      this.setState({
+        activeItem:{
+          id:null,
+          title:'',
+          completed:false,
+        }
+      })
+    }).catch(function(error){
+      console.log("Error: ", error)
+    })
+  }
+
+
+  
+
   render(){
     var tasks = this.state.todoList
     return(
@@ -39,27 +84,37 @@ class App extends React.Component{
 
         <div id="task-container">
           <div id="form-wrapper">
-            <div id="form">
+            <form onSubmit={this.handleSubmit} id="form">
               <div className='flex-wrapper'>
                 <div style={{flex:6}}>
-                  <input className='form-control' id='title' type="text" name="title" placeholder='Add Task title here..'></input>
+                  <input onChange={this.handleChange} className='form-control' id='title' type="text" name="title" placeholder='Add Task title here..'></input>
                 </div>
                 <div style={{flex:1}}>
                   <input id="submit" className='bt btn-warning' type="submit" name='Add'></input>
 
                 </div>
               </div>
-            </div>
+            </form>
           </div>
 
           <div id="list-wrapper">
             {tasks.map(function(task,index){
               return(
                 <div key={index} className='task-wrapper flex-wrapper'>
+                  
+                  <div style={{flex:7}}>
                   <span>{task.title}</span>
+                  </div>
+
+                  <div style={{flex:1}}>
+                  <button className='btn btn-sm btn-outline-info'>Edit</button>
+                  </div>
+
+                  <div style={{flex:1}}>
+                  <button className='btn btn-sm btn-outline-dark delete'>-</button>
+                  </div>
                 </div>
-              )
-            })}
+              )})}
           </div>
 
         </div>
